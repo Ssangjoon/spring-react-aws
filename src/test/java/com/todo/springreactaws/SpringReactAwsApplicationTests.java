982af4ec18f8a9be326fa3c18c3f7d5e924e3eb1
ssang <qkqh34k@gmail.com>
2024-02-15 16:22:48 +0900

    init

---
+++ src/test/java/com/todo/springreactaws/SpringReactAwsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.todo.springreactaws;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringReactAwsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
